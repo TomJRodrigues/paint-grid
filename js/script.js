4b301437934875224a0b0bbef0e2549ed6952c8e
Tom <tomjrodrigues@gmail.com>
2016-12-12 12:15:41 -0800

    Working basic model

--- js/script.js
+++ js/script.js
@@ -1,22 +1,58 @@
+// Global variables
+var containerWidth = 600;
+
+
 $(document).ready(function(){
+	seedContainer();
 	$('#set').click(function(){
 		addBlocks();
 	});
+	paintBlocks();
 });
+
+
+// Factored functions
+function paintBlocks() {
+	$(document).on('mouseenter', '.block', function() {
+		$(this).css('background-color', 'grey');
+	});
+};
+
+function trailBlocks() {
+};
+
+function colorBlocks() {
+	var hue = 120;
+	var saturation = 100;
+	var lightness = 25;
+	$(document).on('mouseenter', '.block', function() {
+		$(this).css('background-color', 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%');
+	});
+};
+
+function seedContainer() {
+	for (i=0; i < 16; i++) {
+			for (k=0; k < 16; k++) {
+				$('#container').append('<div class="block">' + i + '</div');
+			}
+		}
+		$('#container div').css({'width': containerWidth/16, 'height': containerWidth/16});
+};
 
 function addBlocks() {
 	var blocksPerSide = $('input[name=blocksPerSide]').val();
 		$('#container').empty();
 		for (i=0; i < blocksPerSide; i++) {
 			for (k=0; k < blocksPerSide; k++) {
-				$('#container').append('<div>' + i + '</div');
+				$('#container').append('<div class="block">' + i + '</div');
 			}
 		}
-		$('#container div').css({'width': 600/blocksPerSide, 'height': 600/blocksPerSide});
+		$('#container div').css({'width': containerWidth/blocksPerSide, 
+														 'height': containerWidth/blocksPerSide});
 };
 
+
 // TODO
-// Add .empty function to the #set buttton so it will reset each time user clicks. remove reset button
-// Add 16 blocks to the doc.ready function
-// Do mouse-enter and mouse-leave to change them black, later change to custom rules
-// Re-factor javascript to just load a few different functions
+// Set option for trail-effect vs paint-effect
+// Set options for greyscale vs colorscale
+// Add erase function
